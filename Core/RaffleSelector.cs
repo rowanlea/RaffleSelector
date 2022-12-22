14a3cb96f6953664bdc25b5396d45c20cf983503
Rowan Lea <rowan.lea@codurance.com>
2022-12-22 15:38:03 +0000

    Add test files

--- Core/RaffleSelector.cs
+++ Core/RaffleSelector.cs
@@ -2,5 +2,9 @@
 {
     public class RaffleSelector
     {
+        public HashSet<string> DrawEmails(IEnumerable<string> listOfNames, int numberToDraw)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- CoreTests/RaffleSelectorTests.cs
+++ CoreTests/RaffleSelectorTests.cs
@@ -1,4 +1,5 @@
 using Core;
+using FluentAssertions;
 
 namespace CoreTests
 {
@@ -7,9 +8,36 @@ namespace CoreTests
         [Test]
         public void AcceptanceTest()
         {
-            RaffleSelector raffleSelector = new RaffleSelector();
+            // Arrange
+            RaffleSelector raffleSelector = new();
 
-            Assert.Fail();
+            IEnumerable<string> listOfEmails = new List<string>() { "Nacho", "Rowan", "Bob", "Trish", "Roy" };
+            int numberToDraw = 3;
+
+            // Act
+            HashSet<string> drawnEmails = raffleSelector.DrawEmails(listOfEmails, numberToDraw);
+
+            // Assert
+            drawnEmails.Count.Should().Be(3);
+            drawnEmails.Should().Contain("Nacho");
+
         }
+
+        //[Test]
+        //public void AcceptanceTest()
+        //{
+        //    // Arrange
+        //    RaffleSelector raffleSelector = new();
+
+        //    IEnumerable<string> listOfEmails = new List<string>() { "Nacho", "Nacho", "Bob", "Trish", "Roy" };
+        //    int numberToDraw = 3;
+
+        //    // Act
+        //    List<string> drawnNames = raffleSelector.DrawEmails(listOfEmails, numberToDraw);
+
+        //    // Assert
+        //    Assert.That(drawnNames.Count, Is.EqualTo(3));
+        //    Assert.That("", Is.EqualTo(""));
+        //}
     }
 }
