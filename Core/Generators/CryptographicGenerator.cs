5787a333aa92f7eb4c65ae0acdfcf9246ff80bcd
Ignacio Gonzalez <ignacio.gonzalez@codurance.com>
2023-01-13 12:40:32 +0000

    Adding cryptographic random generator

---
+++ Core/Generators/CryptographicGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Security.Cryptography;
+
+namespace Core.Generators
+{
+   
+    public class CryptographicGenerator
+    {
+        public static int GetNumberBetween(int first, int second)
+        {
+            if (second == 0)
+            {
+                return 0;
+            }
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] randomBytes = new byte[4];
+            rng.GetBytes(randomBytes);
+            uint converted = BitConverter.ToUInt32(randomBytes, 0);
+            int randomNumber = (int) ((converted % second) + first);
+            return randomNumber;
+        }
+    }
+}
---
+++ CoreTests/Generators/CryptographicGeneratorTests.cs
@@ -0,0 +1,47 @@
+﻿using Core.Generators;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreTests.Generators
+{
+    internal class CryptographicGeneratorTests
+    {
+        [TestCase(1, 10)]
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        public void GetNumberBetween_WhenFirstAndSecondGiven_ResultBetweenTheTwoNumbers(int firstNumber, int secondNumber)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                int result = CryptographicGenerator.GetNumberBetween(firstNumber, secondNumber);
+                result.Should().BeInRange(firstNumber, secondNumber);
+            }
+        }
+
+        [Test]
+        public void GetNumberBetween_WhenRun1000Times_ResultDistributedEvenly()
+        {
+            Dictionary<int, int> frequencies = new Dictionary<int, int>();
+            for (int i = 0; i < 1000; i++)
+            {
+                int result = CryptographicGenerator.GetNumberBetween(1, 10);
+                if (frequencies.TryGetValue(result, out _))
+                {
+                    frequencies[result]++;
+                }
+                else
+                {
+                    frequencies.Add(result, 1);
+                }
+            }
+            for (int i = 1; i <= 10; i++)
+            {
+                frequencies[i].Should().BeGreaterThan(80);
+            }
+        }
+    }
+}
